918a62f2a25dbd960421b63c757ae8e6645632ef
joyson1415 <147584076+joyson1415@users.noreply.github.com>
2023-10-11 11:17:54 +0530

    Add files via upload

---
+++ table.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class table
+{
+public static void main (String args [])
+{
+    int n=2;
+    int i;
+    for(i=1;i<=10;i++)
+    System.out.println(+n+"*"+i+"="+n*i);
+    
+}
+}
